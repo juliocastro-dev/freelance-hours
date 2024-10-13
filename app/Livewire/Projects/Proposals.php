c42456efab3292a5ba9aee22e9773547be9c310c
Julio Castro <julio.junior.ext@infracommerce.com.br>
2024-10-13 13:47:37 -0300

    add paginação das propostas

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,27 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
 
     public Project $project;
+
+    public int $qty = 10;
+
+    #[Computed()]
+    public function proposals() {
+        return $this->project->proposals()
+            ->orderByDesc('hours')
+            ->simplePaginate($this->qty);
+    }
+
+    public function loadMore()
+    {
+        $this->qty += 10;
+    }
 
     public function render()
     {
